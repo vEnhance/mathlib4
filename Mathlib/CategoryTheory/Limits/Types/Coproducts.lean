77a3027880d2c429421b68fa014b0219f922eb08
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2026-02-09 21:59:26 +0100

    fixup! todo rename

--- Mathlib/CategoryTheory/Limits/Types/Coproducts.lean
+++ Mathlib/CategoryTheory/Limits/Types/Coproducts.lean
@@ -200,6 +200,7 @@ noncomputable def isInitialPEmpty : IsInitial (PEmpty : Type u) :=
 
 @[deprecated (since := "2026-02-08")] alias isInitialPunit := isInitialPEmpty
 
+-- TODO rename
 /-- An object in `Type u` is initial if and only if it is empty. -/
 lemma initial_iff_empty (X : Type u) : Nonempty (IsInitial X) ↔ IsEmpty X := by
   constructor
